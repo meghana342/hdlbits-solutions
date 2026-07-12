504b10b6eec12fcc1b88305817995a265e7a04ac
meghana342 <meghaname0608@gmail.com>
2026-07-12 12:56:05 +0530

    Create Vector5.v

---
+++ verilog_language/Vectors/Vector5.v
@@ -0,0 +1,16 @@
+module top_module (
+	input a, b, c, d, e,
+	output [24:0] out
+);
+
+	wire [24:0] top, bottom;
+	assign top    = { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} };
+	assign bottom = {5{a,b,c,d,e}};
+	assign out = ~top ^ bottom;	// Bitwise XNOR
+
+// Vector 1: aaaaa_bbbbb_ccccc_ddddd_eeeee
+    // Vector 2: abcde_abcde_abcde_abcde_abcde
+    // XNORing them together bit-by-bit gives the 25 comparisons.
+	// assign out = ~{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ {5{a,b,c,d,e}};
+	
+endmodule
