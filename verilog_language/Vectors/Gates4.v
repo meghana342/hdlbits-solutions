d3a2c2846f7ebcd1276e8cab47ec11a258d3369d
meghana342 <meghaname0608@gmail.com>
2026-07-12 12:46:18 +0530

    Update Gates4.v

--- verilog_language/Vectors/Gates4.v
+++ verilog_language/Vectors/Gates4.v
@@ -4,9 +4,12 @@ module top_module(
     output out_or,
     output out_xor
 );
-
-    assign out_and = &in;
-    assign out_or  = |in;
-    assign out_xor = ^in;
+    assign out_and = in[3] & in[2] & in[1] & in[0];
+    assign out_or  = in[3] | in[2] | in[1] | in[0];
+    assign out_xor = in[3] ^ in[2] ^ in[1] ^ in[0];
+    // Unary reduction automatically applies the gate across all bits
+    //assign out_and = &in;
+    //assign out_or  = |in;
+    //assign out_xor = ^in;
 
 endmodule
