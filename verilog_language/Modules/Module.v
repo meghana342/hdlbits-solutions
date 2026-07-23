d8a66c7bc045c7aff6a68f515c3929364cfdeef9
meghana342 <meghaname0608@gmail.com>
2026-07-23 10:39:31 +0530

    Update Module.v

--- verilog_language/Modules/Module.v
+++ verilog_language/Modules/Module.v
@@ -1,1 +1,11 @@
+module top_module ( input a, input b, output out );
+	// Create an instance of "mod_a" named "inst1", and connect ports by name:
+    mod_a instance1( 
+        .in1(a), // Port"in1"connects to wire "a"
+		.in2(b),	// Port "in2" connects to wire "b"
+		.out(out)	// Port "out" connects to wire "out" 
+				// (Note: mod_a's port "out" is not related to top_module's wire "out". 
+				// It is simply coincidence that they have the same name)
+    );
 
+endmodule
